e45b1f7aab78988d153a7b858b99f0c41d5ffd59
Fajri <contact@abfproject.com>
2025-10-26 21:45:45 +0700

    Add package.json and build config files for Netlify deploy

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        abf: '#025370',
+        accent: '#ea909a'
+      }
+    },
+  },
+  plugins: [],
+}
